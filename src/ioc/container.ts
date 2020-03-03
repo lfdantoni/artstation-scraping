d3a50e75e5df4e1837cbc1b39b697b75857a99cf
Leonardod <lfdantoni@gmail.com>
2020-03-03 01:56:38 -0300

    removed unused class

--- src/ioc/container.ts
+++ src/ioc/container.ts
@@ -7,10 +7,6 @@ import { TYPES } from './constants/types';
 import { GDriveService } from '../services/gdrive.service';
 import { GOAuthService } from '../services/goauth.service';
 
-export interface Weapon {
-  hit(): string;
-}
-
 const appContainer = new Container();
 appContainer.bind<GOAuthService>(TYPES.OAuth).to(GOAuthService).inSingletonScope();
 appContainer.bind<GDriveService>(TYPES.FileStorage).to(GDriveService);
