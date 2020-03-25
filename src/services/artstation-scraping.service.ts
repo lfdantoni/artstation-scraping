7910ab1075d7b6a7f3f4ceafcfdad4dde7388c52
Leonardod <lfdantoni@gmail.com>
2020-03-24 21:06:32 -0300

    improved worker job

--- src/services/artstation-scraping.service.ts
+++ src/services/artstation-scraping.service.ts
@@ -1,4 +1,4 @@
-import { launch } from 'puppeteer';
+import { launch, Browser, Page } from 'puppeteer';
 import { saveImage, sleep, createArtistFolder, autoScroll } from '../utils';
 import { Config } from '../config';
 
@@ -20,26 +20,14 @@ export class ArtStationScrapingService {
   private height = 768
   private userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.100 Safari/537.36';
   private url = Config.url;
+  private imagePathsSelector = 'user-projects:not(.ng-hide) .project-image';
 
   constructor(private config: ScrapingProcessOptions) { }
 
   public async process() {
-    // tslint:disable-next-line: no-console
-    console.log('artist: ', this.config.artistId);
+    const browser = await this.initBrowser();
 
-    const browser = await launch({
-      headless: true,
-      defaultViewport: null,
-      args: [
-        `--window-size=${ this.width },${ this.height }`,
-        '--no-sandbox',
-        '--disable-setuid-sandbox'
-      ],
-    });
-
-    const page = await browser.newPage();
-
-    await page.setUserAgent(this.userAgent);
+    const page = await this.newPage(browser);
 
     await page.goto(`${this.url}/${this.config.artistId}`, {
       waitUntil: 'networkidle0',
@@ -47,7 +35,7 @@ export class ArtStationScrapingService {
 
     await autoScroll(page);
 
-    const imageThumbs = await page.$$('user-projects:not(.ng-hide) .project-image');
+    const imageThumbs = await page.$$(this.imagePathsSelector);
 
     const folder = createArtistFolder(this.config.artistId);
 
@@ -85,6 +73,101 @@ export class ArtStationScrapingService {
     await browser.close();
 
     await this.processFinished();
+  }
+
+  async getImageUrls(): Promise<string[]> {
+    // tslint:disable-next-line: no-console
+    console.log('getImageUrls')
+
+    const results: string[] = [];
+    const browser = await this.initBrowser();
+
+    const page = await this.newPage(browser);
+
+    await page.goto(`${this.url}/${this.config.artistId}`, {
+      waitUntil: 'networkidle0',
+    });
+
+    await autoScroll(page);
+
+    const imageThumbs = await page.$$(this.imagePathsSelector);
+
+    for(const imageThumb of imageThumbs) {
+      const href = await imageThumb.getProperty('href');
+      const anchorHrefValue = (await href.jsonValue()) as string;
+
+      results.push(anchorHrefValue);
+    }
+
+    // tslint:disable-next-line: no-console
+    console.log('getImageUrls urls: '+ results.length)
+
+    await browser.close();
+
+    return Promise.resolve(results);
+  }
+
+  async getImagesFromPages(urls: string[]) {
+    // tslint:disable-next-line: no-console
+    console.log('getImagesFromPages')
+    const browser = await this.initBrowser();
+
+    const page = await this.newPage(browser);
+
+    const folder = createArtistFolder(this.config.artistId);
+
+    for(const url of urls) {
+      await page.goto(url);
+
+      const anchorsSelector = '.asset-actions a:first-child';
+      await page.waitForSelector(anchorsSelector);
+      const anchors = await page.$$(anchorsSelector);
+
+      for(const anchor of anchors) {
+
+        const anchorHref = await anchor.getProperty('href');
+        const anchorHrefValue = await anchorHref.jsonValue() as string;
+
+        // Waiting for each download (0s - 5s)
+        const waitTime = Math.floor(Math.random() * Math.floor(5000));
+        await sleep(waitTime);
+
+        const response = await saveImage(anchorHrefValue, folder);
+        await this.imageSaved(response.fileName, response.relativeFilePath);
+      }
+    };
+
+    // tslint:disable-next-line: no-console
+    console.log('getImagesFromPages: ' + urls.length)
+
+    await browser.close();
+
+    await this.processFinished();
+  }
+
+  private async initBrowser(): Promise<Browser> {
+    // tslint:disable-next-line: no-console
+    console.log('artist: ', this.config.artistId);
+
+    const browser = await launch({
+      headless: true,
+      defaultViewport: null,
+      args: [
+        `--window-size=${ this.width },${ this.height }`,
+        '--no-sandbox',
+        '--disable-setuid-sandbox'
+      ],
+    });
+
+    return Promise.resolve(browser);
+  }
+
+  private async newPage(browser: Browser): Promise<Page> {
+    const page = await browser.newPage();
+
+    await page.setUserAgent(this.userAgent);
+
+    return Promise.resolve(page);
   }
 
   private async imageSaved(imageName: string, imagePath: string) {
--- src/worker/index.ts
+++ src/worker/index.ts
@@ -22,6 +22,7 @@ const workers = process.env.WEB_CONCURRENCY || 2;
 // responses it can be much higher. If each job is CPU-intensive, it might need
 // to be much lower.
 const maxJobsPerWorker = 50;
+const imagesPerJob = 7;
 
 function sleep(ms: number) {
   return new Promise(resolve => setTimeout(resolve, ms));
@@ -29,89 +30,180 @@ function sleep(ms: number) {
 
 function start() {
   // Connect to the named work queue
+
+  // TODO: Create different QUEUE!!!!
   const workQueue = new QueueClass('worker', REDIS_URL);
 
   // tslint:disable-next-line: no-console
   console.log('Worker connected')
 
   workQueue.process(maxJobsPerWorker, async (job) => {
-    // tslint:disable-next-line: no-console
-    console.log(job.data)
-
-    MongooseDB.connect()
-    .then(async() => {
-      const userService = appContainer.get<UserService>(TYPES.UserService);
-      const fileStorage = appContainer.get<GDriveService>(TYPES.FileStorage);
+    if(job.data.urls) {
       // tslint:disable-next-line: no-console
-      console.log(job.id)
+      console.log(job.data)
+      const urls: string[] = job.data.urls;
+      const urlsToProcess = urls.slice(0, imagesPerJob);
+      let artistFolderId: string = job.data.artistFolderId;
+
+      await MongooseDB.connect()
+      .then(async() => {
+        const userService = appContainer.get<UserService>(TYPES.UserService);
+        const fileStorage = appContainer.get<GDriveService>(TYPES.FileStorage);
+        // tslint:disable-next-line: no-console
+        console.log(job.id)
 
 
-      const {artistId, userId, createRootFolder} = job.data;
-      let rootFolderId = '';
+        const {artistId, userId, createRootFolder} = job.data;
+        let rootFolderId = '';
 
-      if (!artistId || !userId) {
-        throw new Error('Invalid user or artist');
-      }
+        if (!artistId || !userId) {
+          throw new Error('Invalid user or artist');
+        }
 
-      const userData = await userService.getUserWithCredential(userId);
+        const userData = await userService.getUserWithCredential(userId);
 
-      if (!userData) {
-        throw new Error('Invalid user');
-      }
+        if (!userData) {
+          throw new Error('Invalid user');
+        }
 
 
-      // TODO save the folderId to db
-      if (createRootFolder && createRootFolder.toString() === 'true') {
-        rootFolderId = await fileStorage.createFolder('ArtStationScrapingApp', userData.credential);
+        // TODO save the folderId to db
+        if (createRootFolder && createRootFolder.toString() === 'true') {
+          rootFolderId = await fileStorage.createFolder('ArtStationScrapingApp', userData.credential);
+        } else {
+          // TODO remove this
+          rootFolderId = '1CWFoXbhTjGKxOaBofIdii7P5v-lbbKqZ';
+        }
+
+        artistFolderId = artistFolderId ? artistFolderId : await fileStorage.createFolder(artistId, userData.credential, rootFolderId);
+
+        const serviceOptions: ScrapingProcessOptions = {
+          artistId,
+          updateCallback: async (state: ImageState) => {
+            if (state.finish) {
+              // tslint:disable-next-line: no-console
+              console.log('downloadGallery: ', state.log);
+              return;
+            }
+
+            await fileStorage.uploadFile(artistFolderId, state.imagePath, state.imageName, userData.credential);
+
+            // tslint:disable-next-line: no-console
+            console.log('downloadGallery: ', state);
+          }
+        }
+
+        await (new ArtStationScrapingService(serviceOptions)).getImagesFromPages(urlsToProcess);
+
+        return Promise.resolve();
+      });
+
+      const nextUrls: string[] = job.data.urls.slice(imagesPerJob, job.data.urls.length);
+
+      if(nextUrls.length > 0) {
+        const nextJob = await workQueue.add({
+          artistId: job.data.artistId,
+          userId: job.data.userId,
+          createRootFolder:  job.data.createRootFolder,
+          artistFolderId,
+          urls: nextUrls
+        });
+
+        return Promise.resolve({ value: 'nextJob '+ nextJob.id });
+
       } else {
-        // TODO remove this
-        rootFolderId = '1CWFoXbhTjGKxOaBofIdii7P5v-lbbKqZ';
+
+        return Promise.resolve({ value: 'download finished '+ job.data.artistId });
       }
 
-      const artistFolderId = await fileStorage.createFolder(artistId, userData.credential, rootFolderId);
+
+    } else {
+
+      const {artistId} = job.data;
 
       const serviceOptions: ScrapingProcessOptions = {
         artistId,
         updateCallback: async (state: ImageState) => {
-          if (state.finish) {
-            // tslint:disable-next-line: no-console
-            console.log('downloadGallery: ', state.log);
-            return;
-          }
-
-          await fileStorage.uploadFile(artistFolderId, state.imagePath, state.imageName, userData.credential);
-
           // tslint:disable-next-line: no-console
           console.log('downloadGallery: ', state);
         }
       }
 
-      await (new ArtStationScrapingService(serviceOptions)).process();
+      const urls = await (new ArtStationScrapingService(serviceOptions)).getImageUrls();
 
-      return { value: 'process '+ artistId };
+      const nextJob = await workQueue.add({
+        artistId: job.data.artistId,
+        userId: job.data.userId,
+        createRootFolder:  job.data.createRootFolder,
+        urls
+      });
 
-      // // This is an example job that just slowly reports on progress
-      // // while doing no work. Replace this with your own job logic.
-      // let progress = 0;
+      return  Promise.resolve({ value: 'nextJob from workQueue '+ nextJob.id });
+    }
 
-      // // throw an error 5% of the time
-      // if (Math.random() < 0.05) {
-      //   throw new Error('This job failed!')
-      // }
+  })
 
-      // while (progress < 100) {
-      //   await sleep(50);
-      //   progress += 1;
-      //   job.progress(progress)
-      // }
+  // workQueue.process(maxJobsPerWorker, async (job) => {
+  //   // tslint:disable-next-line: no-console
+  //   console.log(job.data)
 
-      // // A job can return values that will be stored in Redis as JSON
-      // // This return value is unused in this demo application.
-      // return { value: 'This will be stored' };
-    });
-  });
+  //   MongooseDB.connect()
+  //   .then(async() => {
+  //     const userService = appContainer.get<UserService>(TYPES.UserService);
+  //     const fileStorage = appContainer.get<GDriveService>(TYPES.FileStorage);
+  //     // tslint:disable-next-line: no-console
+  //     console.log(job.id)
+
+
+  //     const {artistId, userId, createRootFolder} = job.data;
+  //     let rootFolderId = '';
+
+  //     if (!artistId || !userId) {
+  //       throw new Error('Invalid user or artist');
+  //     }
+
+  //     const userData = await userService.getUserWithCredential(userId);
+
+  //     if (!userData) {
+  //       throw new Error('Invalid user');
+  //     }
+
+
+  //     // TODO save the folderId to db
+  //     if (createRootFolder && createRootFolder.toString() === 'true') {
+  //       rootFolderId = await fileStorage.createFolder('ArtStationScrapingApp', userData.credential);
+  //     } else {
+  //       // TODO remove this
+  //       rootFolderId = '1CWFoXbhTjGKxOaBofIdii7P5v-lbbKqZ';
+  //     }
+
+  //     const artistFolderId = await fileStorage.createFolder(artistId, userData.credential, rootFolderId);
+
+  //     const serviceOptions: ScrapingProcessOptions = {
+  //       artistId,
+  //       updateCallback: async (state: ImageState) => {
+  //         if (state.finish) {
+  //           // tslint:disable-next-line: no-console
+  //           console.log('downloadGallery: ', state.log);
+  //           return;
+  //         }
+
+  //         await fileStorage.uploadFile(artistFolderId, state.imagePath, state.imageName, userData.credential);
+
+  //         // tslint:disable-next-line: no-console
+  //         console.log('downloadGallery: ', state);
+  //       }
+  //     }
+
+  //     await (new ArtStationScrapingService(serviceOptions)).process();
+
+  //     return { value: 'process '+ artistId };
+
+  //   });
+  // });
 }
 
 // Initialize the clustered worker process
 // See: https://devcenter.heroku.com/articles/node-concurrency for more info
-throng({ workers, start });
+start();
+// throng({ workers, start });
